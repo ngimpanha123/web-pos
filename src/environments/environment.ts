8714de1f325ea0bf54e132581055bf04e5b65c72
Khouch Koeun <khouch.koeun@gmail.com>
2024-03-17 09:34:50 +0700

    modify enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 
     production: false,
-    API_BASE_URL             : 'http://localhost:8002/api',
-    FILE_PUBLIC_BASE_URL     : 'http://localhost:8003/'
+    API_BASE_URL             : 'http://localhost:8103/api',
+    FILE_PUBLIC_BASE_URL     : 'http://localhost:8104/'
 
 };
